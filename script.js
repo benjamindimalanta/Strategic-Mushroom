3a6be7ea1af6372c7ec196860b37e85f4d73ce0c
benjamindimalanta <66483348+benjamindimalanta@users.noreply.github.com>
2025-06-23 13:29:04 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+const video = document.getElementById('camera');
+const canvas = document.getElementById('snapshot');
+const download = document.getElementById('download');
+
+navigator.mediaDevices.getUserMedia({ video: true })
+  .then(stream => {
+    video.srcObject = stream;
+  });
+
+document.getElementById('snap').onclick = () => {
+  canvas.width = video.videoWidth;
+  canvas.height = video.videoHeight;
+  canvas.getContext('2d').drawImage(video, 0, 0);
+  download.href = canvas.toDataURL('image/png');
+};
 
